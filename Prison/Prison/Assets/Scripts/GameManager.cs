38bcd1515fa0d1251f488b05083b1faf5309cbad
Max Sabirov <maxim-sabirov@rambler.ru>
2021-03-19 18:17:21 +0200

    fb

---
+++ Prison/Prison/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isElectricityLeverOn;
+    public GameObject lights;
+
+    private void Awake()
+    {
+        BlackBoard.gameManager = this;
+    }
+
+    void Start()
+    {
+        isElectricityLeverOn = false;
+    }
+
+    void Update()
+    {
+        if (isElectricityLeverOn)
+        {
+            lights.GetComponent<Light2D>().intensity = 0.5f;
+        }
+    }
+}
---
+++ Prison/Prison/Assets/Scripts/Lever.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    bool isPlayerNearLever;
+
+    void Start()
+    {
+        isPlayerNearLever = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerNearLever)
+        {
+            BlackBoard.gameManager.isElectricityLeverOn = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            isPlayerNearLever = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            isPlayerNearLever = false;
+        }
+    }
+}
